b439108cd346593dae5d3de8b3b4e2e212235aa2
HalvorLande <halvor.s.lande@gmail.com>
2025-09-15 11:00:06 +0200

    tactical debug changes

--- PEN/Genesis.lean
+++ PEN/Genesis.lean
@@ -392,9 +392,9 @@ def dumpAllCandidatesAt (τtarget : Nat) : List String :=
      s!"ρ={e.report.rho}  κ={e.report.kX}  ν={e.report.nu}  Δ={e.report.rho - bar}   X={nameOfX e.x.targets}"))
 
 
- #eval demo_rows 3
+-- #eval demo_rows 5
 -- #eval dumpAllCandidatesAt 1
 -- #eval dumpAllCandidatesAt 2
--- #eval dumpAllCandidatesAt 3
+#eval dumpAllCandidatesAt 4
 
 end PEN.Genesis
