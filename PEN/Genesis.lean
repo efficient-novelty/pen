430e6e80429fd521521589167f3f9637310f81e0
HalvorLande <halvor.s.lande@gmail.com>
2025-09-16 10:09:33 +0200

    debug

--- PEN/Genesis.lean
+++ PEN/Genesis.lean
@@ -392,7 +392,7 @@ def dumpAllCandidatesAt (τtarget : Nat) : List String :=
      s!"ρ={e.report.rho}  κ={e.report.kX}  ν={e.report.nu}  Δ={e.report.rho - bar}   X={nameOfX e.x.targets}"))
 
 
--- #eval demo_rows 8
+#eval demo_rows 8
 -- #eval dumpAllCandidatesAt 3
 -- #eval dumpAllCandidatesAt 2
 #eval dumpAllCandidatesAt 8
